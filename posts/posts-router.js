ef29909fc160e1d944ec4a718ab5c92a1ca99f87
kly001 <karenly1961@gmail.com>
2019-11-02 01:21:02 -0400

    GET "/:id"

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,7 +8,21 @@ router.get("/" ,(req,res) => {
         res.status(200).json(posts)
     })
     .catch(err => {
-        res.status(500).json({message:"Error retrieving the posts."})
+        res.status(500).json({message:"The posts information could not be retrieved."})
+    })
+})
+
+router.get("/:id",(req,res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message:"The post with the specified ID does not exist"})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: "The post information could not be retrieved."})
     })
 })
 
